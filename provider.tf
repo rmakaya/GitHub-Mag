7484dd56cfa6b6114cd3e69b915cea5fa3340058
Roland <83996848+rmakaya@users.noreply.github.com>
2024-02-28 12:41:06 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,11 +8,6 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
-
-  subscription_id = "e1083a17-cb18-4b68-a817-9b9891e6ccfa"
-  client_id       = "e4837193-121d-4948-8cc0-b68b75aa1bd6"
-  client_secret   = "45J8Q~u11NFJOjTnNXs-gVlyEPrZieIoisBAzc6."
-  tenant_id       = "b28daeff-a0a3-4cf0-91ac-b27339661bd0"  
+  # Configuration options
 }
 
