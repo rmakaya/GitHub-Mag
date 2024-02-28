4c37434b1c4d49d0a802016e15d59e2a082fd03a
Roland <83996848+rmakaya@users.noreply.github.com>
2024-02-28 13:43:41 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,1 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.93.0"
-    }
-  }
-}
 
-provider "azurerm" {
-  # Configuration options
-features {}
-}
