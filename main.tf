ec2693b83943e78481323e838b9ca66f00eeab82
Roland <83996848+rmakaya@users.noreply.github.com>
2024-03-10 16:42:05 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,9 @@
-resource "azurerm_resource_group" "CR460_Roland" {
-  name     = "CR460_Roland-resources"
-  location = "East US"
-}
+terraform {
+  cloud {
+    organization = "rmakaya"
 
-resource "azurerm_container_registry" "acr" {
-  name                = "developmentteam"
-  resource_group_name = azurerm_resource_group.CR460_Roland.name
-  location            = azurerm_resource_group.CR460_Roland.location
-  sku                 = "Premium"
-  admin_enabled       = false
-  georeplications {
-    location                = "West Europe"
-    zone_redundancy_enabled = true
-    tags                    = {}
-  }
-  georeplications {
-    location                = "North Europe"
-    zone_redundancy_enabled = true
-    tags                    = {}
+    workspaces {
+      name = "GitHub-Mag_Terra2"
+    }
   }
 }
